24e0c3a157ee2f442e4c038880e57bb691d1d8ec
Keith <keithpacana@berkeley.edu>
2018-09-20 19:43:28 -0700

    hw1

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,15 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a = a.map { |elem| Integer(elem)}
+    a = a.map { |elem| elem +2}
+    a = a.uniq
+    x = 0
+    for el in a 
+    	if (el%2 == 0) && (el < 10)
+    		x += el
+    	end
+    end
+  x
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,17 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  (a+b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a = a.sort
+  a = a.map {|item| item + 1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  first_name + " " + last_name
 
 end
 
@@ -49,4 +51,7 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  word.split("").map{|elem| values[elem.to_sym]}.inject(:+)
+
+
 end
